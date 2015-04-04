481c93ebeb77f96f42e25595e45dd2b7c4abd770
Michael Martin <mwm.cal@gmail.com>
2015-04-03 23:43:19 -0700

    gaah

--- src/IdentityServer3/Program.cs
+++ src/IdentityServer3/Program.cs
@@ -7,7 +7,7 @@ namespace AspNet5Host
     {
         static void Main(string[] args)
         {
-            string url = "http://localhost:5004";
+            string url = "http://192.168.1.20:5004";
             using (WebApp.Start<Startup_Mono>(url))
             {
                 Console.WriteLine("Press Enter to exit...");
