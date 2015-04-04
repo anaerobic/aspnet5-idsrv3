436bddd9c7ba6236d4d28d1dee4dc512568ea51d
Michael Martin <mwm.cal@gmail.com>
2015-04-03 23:35:57 -0700

    gonna try to trick it into using k run

---
+++ src/IdentityServer3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.Owin.Hosting;
+
+namespace AspNet5Host
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://localhost:5004";
+            using (WebApp.Start<Startup_Mono>(url))
+            {
+                Process.Start(url); // Launch the browser.
+                Console.WriteLine("Press Enter to exit...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
